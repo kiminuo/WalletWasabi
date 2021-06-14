316e7a6b89d6a35a4a223b6a9c86396889e2b70b
Kiminuo <kiminuo@protonmail.com>
2021-06-14 08:00:13 +0200

    CS

--- WalletWasabi/Tor/TorMonitor.cs
+++ WalletWasabi/Tor/TorMonitor.cs
@@ -1,9 +1,6 @@
 using Microsoft.Extensions.Caching.Memory;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Bases;
